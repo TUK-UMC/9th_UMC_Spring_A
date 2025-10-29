90bbac69665259311462a34dc821dea1b03bfc71
송현수 <songhyunsoo@songhyeonsuui-MacBookAir.local>
2025-10-29 23:46:07 +0900

    chap05

---
+++ src/main/java/com/example/umc9th/domain/member/repository/mapping/MemberMissionRepository.java
@@ -0,0 +1,32 @@
+package com.example.umc9th.domain.member.repository.mapping;
+
+import com.example.umc9th.domain.member.entity.mapping.MemberMission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Value;
+
+//MemberMission 엔티티용 리포지토리
+public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
+
+
+    interface MyMissionView {
+        // 성공여부: MemberMission.isComplete
+        Boolean getIsComplete();
+
+        // 포인트: Mission.point
+        @Value("#{target.mission.point}")
+        Integer getPoint();
+
+        // 미션명(조건): Mission.condition
+        @Value("#{target.mission.condition}")
+        String getCondition();
+
+        // 가게 이름: Mission.store.name
+        @Value("#{target.mission.store.name}")
+        String getStoreName();
+    }
+
+    // 최신순 페이징
+    Page<MyMissionView> findByMember_IdOrderByCreatedAtDesc(Long memberId, Pageable pageable);
+}
