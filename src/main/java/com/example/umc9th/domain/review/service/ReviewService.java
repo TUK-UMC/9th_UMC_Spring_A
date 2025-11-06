600dc51b91da683078dba5e83b288c98d1eebd72
송현수 <songhyunsoo@songhyeonsuui-MacBookAir.local>
2025-11-06 23:56:24 +0900

    Reivew Mission

--- src/main/java/com/example/umc9th/domain/review/service/ReviewService.java
+++ src/main/java/com/example/umc9th/domain/review/service/ReviewService.java
@@ -1,35 +1,19 @@
 package com.example.umc9th.domain.review.service;
 
-import com.example.umc9th.domain.member.entity.Member;
-import com.example.umc9th.domain.review.entity.Review;
+import com.example.umc9th.domain.review.dto.ReviewDto;
+import com.example.umc9th.domain.review.enums.ReviewEnum;
 import com.example.umc9th.domain.review.repository.ReviewRepository;
-import com.example.umc9th.domain.store.entity.Store;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class ReviewService {
+    private final ReviewRepository repo;
 
-    private final ReviewRepository reviewRepository;
-    private final EntityManager em;  // FK 프록시 생성용
-
-    public Long writeReview(Long memberId, Long storeId, String content, Float rate) {
-
-        Member memberRef = em.getReference(Member.class, memberId);
-        Store storeRef = em.getReference(Store.class, storeId);
-
-        // 리뷰 엔티티 생성 및 저장
-        Review review = Review.builder()
-                .content(content)
-                .rate(rate)
-                .member(memberRef)
-                .store(storeRef)
-                .build();
-
-        return reviewRepository.save(review).getId();
+    public Page<ReviewDto> getMyReviews(Long me, String storeName, String starBucket, Pageable pageable) {
+        return repo.findMyReviews(me, storeName, ReviewEnum.from(starBucket), pageable);
     }
 }
